ec739d9a695b0cd71f250bdad226470b3865d0af
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-15 12:37:10 -0600

    setup sql and app js setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE if EXISTS users;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    first_name TEXT,
+    last_name TEXT
+);
