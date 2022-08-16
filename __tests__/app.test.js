7fc6720c356271db90a241c0f6af80f60895310a
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-16 15:10:32 -0600

    shows list of users for auth members test wrote and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Karen',
   lastName: 'Pumpernickle',
   email: 'imimportant@admin.com',
   password: 'shutItDown'
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('not yelp backend', () => {
@@ -34,6 +44,13 @@ describe('not yelp backend', () => {
     await request(app).post('/api/v1/users/').send(mockUser);
     const res = await request(app).post('/api/v1/users/sessions').send({ email: 'imimportant@admin.com', password: 'shutItDown' });
     expect(res.status).toEqual(200);
+  });
+
+
+  it('shows list of users for authenticated members only', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/v1/users');
+    expect(res.status).toBe(200);
   });
 
 
